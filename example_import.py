dad775a0d046f85f900c9eef529be3290996f8ec
Arghya Dutta <argphy@gmail.com>
2026-08-24 10:34:00 +0530

    Last updated on 2026-08-24 10:34:00

--- example_import.py
+++ example_import.py
@@ -1,8 +1,12 @@
-import example_main
+# import example_main
 
-def main():
-    # Call a function from the imported module
-    print(example_main.greet("Bob"))
+# def main():
+#     # Call a function from the imported module
+#     print(example_main.greet("Bob"))
 
-if __name__ == "__main__":
-    main()
+# if __name__ == "__main__":
+#     main()
+
+import sum_module as sm
+
+print(sm.sum(3,3))
